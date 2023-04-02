192f224c7d21e4e8a117c0fd810017e07b0651fa
Дмитрий Бунтов <tmplz@yandex.ru>
2023-04-02 16:45:46 +0300

    done hw3

---
+++ HW3.js
@@ -0,0 +1,54 @@
+/* Задание 1
+let i = 0;
+while (i < 2) {
+    console.log (`Привет`);
+    i++;
+}
+*/
+
+/* Задание 2
+let i = 1;
+while (i <= 5) {
+    console.log (i);
+    i++;
+}
+*/
+
+/* Задание 3
+let i = 7;
+while (i <= 22) {
+    console.log (i);
+    i++;
+}
+*/
+
+/* Задание 4
+const obj = {
+    Коля: 200,
+    Вася: 300,
+    Петя: 400,
+}
+
+for (const key in obj) {
+    console.log (`${key} - зарплата ${obj[key]} долларов`);
+}
+*/
+
+/* Задание 5
+for (let n = 1000, num = 5; n > 50; num++) {
+    n /= 2;
+    console.log (n);
+}
+*/
+
+/* Задание 6
+for (let dayNumber = 1; dayNumber <= 25; dayNumber++) {
+    dayNumber = dayNumber + 6;
+    console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`);
+}
+*/
+
+
+
+
+
